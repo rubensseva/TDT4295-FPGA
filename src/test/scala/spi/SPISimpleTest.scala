eff08d015f36ee18559c2d69a9b76c5a3608e8dc
Ruben S. Sevaldson <rubenss@stud.ntnu.no>
2020-10-05 16:52:50 +0200

    Add delayed cycle dev component

--- src/test/scala/spi/SPISimpleTest.scala
+++ src/test/scala/spi/SPISimpleTest.scala
@@ -8,16 +8,30 @@ import chisel3.iotesters
 import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
 
 
-class SPISimpleTester(dut: SPI) extends PeekPokeTester(dut) {
-  poke(dut.io.input, 5)
+class DelayedCycelDevTester(dut: DelayedCycleDev) extends PeekPokeTester(dut) {
+  println("poke");
+  poke(dut.io.testIn, 3)
+  println("Prev clk: " + peek(dut.io.testPrevVal).toString)
+  println("Curr clk: " + peek(dut.io.testCurrVal).toString)
+  println("step");
   step (1)
-  println("Result is: " + peek(dut.io.output).toString)
+  println("Prev clk: " + peek(dut.io.testPrevVal).toString)
+  println("Curr clk: " + peek(dut.io.testCurrVal).toString)
+  println("poke");
+  poke(dut.io.testIn, 6)
+  println("Prev clk: " + peek(dut.io.testPrevVal).toString)
+  println("Curr clk: " + peek(dut.io.testCurrVal).toString)
+  println("step");
+  step (1)
+  println("Prev clk: " + peek(dut.io.testPrevVal).toString)
+  println("Curr clk: " + peek(dut.io.testCurrVal).toString)
+
 }
 
 
-object SPISimpleTester extends App {
-  chisel3. iotesters .Driver (() => new SPI ()) { c =>
-    new SPISimpleTester (c)
+object DelayedCycelDevTester extends App {
+  chisel3. iotesters .Driver (() => new DelayedCycleDev ()) { c =>
+    new DelayedCycelDevTester (c)
   }
 }
 
