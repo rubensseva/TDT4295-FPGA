84a8fef164472561c7935e7635bf771b82afc672
chrisvibe <vibechris@gmail.com>
2020-11-05 14:46:08 +0100

    checkpoint

--- compiled_verilog/compile.sh
+++ compiled_verilog/compile.sh
@@ -2,7 +2,7 @@
 rm *.v *.fir *.json
 cd ..
 # sbt "run ImageProcessingDriver -td compiled_verilog"
-# sbt "runMain morphinator.ImageProcessingDriver -td compiled_verilog"
-sbt "runMain morphinator.VideoBufferDriver -td compiled_verilog"
+sbt "runMain morphinator.ImageProcessingDriver -td compiled_verilog"
+# sbt "runMain morphinator.VideoBufferDriver -td compiled_verilog"
 cd compiled_verilog
 rm *.fir *.json
